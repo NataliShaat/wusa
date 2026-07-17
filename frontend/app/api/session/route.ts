34057161f061ef5750153c6653d5983dbc23f6f5
Natali <shaathnatali@gmail.com>
2026-07-17 07:09:00 +0300

    Revert "Add deploy checklist and serverless API files"
    This reverts commit 6186d65b3d2763831622a5dc56c53def8f3aa180.

--- frontend/app/api/session/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { buildSessionResponse } from "@/lib/server";
-
-export async function POST() {
-  return NextResponse.json(await buildSessionResponse());
-}
